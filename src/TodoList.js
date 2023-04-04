a479b1fde49ce075dce1a82016b8f231b80e0b54
Ahmed Muyen Kamran <akamran@colgate.edu>
2023-04-04 22:59:10 +0000

    todo list app barebpnes

---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+function TodoList({todos, deleteTodo}){
+    return (
+        <ul>
+            {todos.map((todo, index) => (
+                <li key={index}>{todo}
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+    );
+}
+export default TodoList;
